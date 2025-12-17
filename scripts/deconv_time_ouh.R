43fb1a4fcc6152c404427be3a913901a853a040e
Xiaokang Zhang <xiaokang.zhang@merckgroup.com>
2025-12-17 14:37:48 +0800

    update for new data format

--- scripts/deconv_time_ouh.R
+++ scripts/deconv_time_ouh.R
@@ -5,15 +5,16 @@ rm(list=ls())
 library(immunedeconv)
 library(tibble)
 
-load("../data/prad_data_ouh.RData")
 # load("../data/prad_data_ouh_bak.RData")
+load("../data/prad_data_ouh.RData")
 
 res.deconv.ouh <- deconvolute(df.gene.ouh.tpm, "epic", tumor = TRUE)
 res.deconv.ouh <- column_to_rownames(res.deconv.ouh, names(res.deconv.ouh)[1])
 
 df.sample.deconv <- data.frame(t(res.deconv.ouh))
 
-time.epic.bcr <- merge(df.sample.deconv, df.bcr.ouh[, c("clinical_outcome", "days_to_event")], by = "row.names")
+# time.epic.bcr <- merge(df.sample.deconv, df.bcr.ouh[, c("clinical_outcome", "days_to_event")], by = "row.names")
+time.epic.bcr <- merge(df.sample.deconv, df.bcr.ouh[, c("Event", "DaysToEvent")], by = "row.names")
 time.epic.bcr <- column_to_rownames(time.epic.bcr, colnames(time.epic.bcr)[1])
 
 write.csv(time.epic.bcr, "../data/time_epic_bcr_ouh.csv")
